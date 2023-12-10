5ad03324ef30ff352610c58e7ea55da1dff9283d
anik_ad78 <Anikinworkplace@gmail.com>
2023-12-10 16:36:45 +0100

    add redirect to custom link

--- app.ts
+++ app.ts
@@ -9,6 +9,7 @@ app.get('/', (req, res) => {
         title: 'Some long custom title',
         description: 'Some custom description Some custom description',
         imageUrl: 'https://photographylife.com/wp-content/uploads/2017/01/Best-of-2016-Nasim-Mansurov-20.jpg',
+        customLink: 'url=http://www.custom-link.com'
     }
     res.render('index', data);
 });
