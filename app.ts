cb0e4c6cf6d62e4612a3101adaddb3930f4bcdee
anik_ad78 <Anikinworkplace@gmail.com>
2023-12-10 16:29:10 +0100

    init app

---
+++ app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(process.env.PORT || PORT, () => {
+    return console.log(`Express is listening at http://localhost:${PORT}`);
+});
