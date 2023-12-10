59fde270c469bfa97d502325d4b2fbfaf9784f74
anik_ad78 <Anikinworkplace@gmail.com>
2023-12-10 18:26:24 +0100

    add post and typification

--- app.ts
+++ app.ts
@@ -1,18 +1,36 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import {IPreviewData} from "./types";
 const app = express();
 const PORT = 3000;
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    const data = {
-        title: 'Some long custom title',
-        description: 'Some custom description Some custom description',
-        imageUrl: 'https://photographylife.com/wp-content/uploads/2017/01/Best-of-2016-Nasim-Mansurov-20.jpg',
-        customLink: 'url=http://www.custom-link.com'
-    }
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+const data:IPreviewData = {
+    title: 'Some long custom title',
+    description: 'Some custom description Some custom description',
+    imageUrl: 'https://photographylife.com/wp-content/uploads/2017/01/Best-of-2016-Nasim-Mansurov-20.jpg',
+    customLink: 'url=http://www.custom-link.com'
+}
+
+app.get('/', (req: express.Request, res: express.Response): void => {
     res.render('index', data);
 });
+
+app.post('/set-link-config', (req: express.Request, res :express.Response): void => {
+    const { title, description, imageUrl, customLink }: IPreviewData = req.body || {};
+    data.title = title || data.title;
+    data.description = description || data.description;
+    data.imageUrl = imageUrl || data.imageUrl;
+    data.customLink = customLink || data.customLink;
+
+    res.set(req.body)
+    res.sendStatus(200);
+})
 
 app.listen(process.env.PORT || PORT, () => {
     return console.log(`Express is listening at http://localhost:${PORT}`);
---
+++ types.ts
@@ -0,0 +1,6 @@
+export interface IPreviewData {
+    title?: string;
+    description?: string;
+    imageUrl?: string;
+    customLink?: string;
+}
