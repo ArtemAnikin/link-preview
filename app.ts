065df548bbd6557bd69c6b3345f70b22a8775302
anik_ad78 <Anikinworkplace@gmail.com>
2024-06-04 18:11:31 +0200

    video

--- app.ts
+++ app.ts
@@ -12,7 +12,7 @@ app.use(bodyParser.json())
 
 app.get('/:videoId', (req: express.Request, res: express.Response): void => {
 	const videoId = req.params['videoId']
-	const frameLink = 'http://' + req.get('host') + '/player/' + videoId + '.mp4'
+	const frameLink = 'https://' + req.get('host') + '/player/' + videoId + '.mp4'
 	res.render('index', { frameLink })
 })
 
