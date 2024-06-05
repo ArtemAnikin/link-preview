d9934fb8601c35202af50575b8b16200d0efeace
anik_ad78 <Anikinworkplace@gmail.com>
2024-06-05 12:36:31 +0200

    video

--- app.ts
+++ app.ts
@@ -11,30 +11,25 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.get('/video', (req: express.Request, res: express.Response): void => {
-	const videoLink = req.query['videoLink']
-	res.render('index', { videoLink })
+	res.render('index')
 })
 
-// app.get('/player', (req: express.Request, res: express.Response): void => {
-// 	const url = req.query['url']
-// 	console.log('==url', url)
-//
-// 	res.json({
-// 		height: 1280,
-// 		html: `<iframe class="streamable-embed" src="${url}" frameborder="0" scrolling="no" width="720" height="1280" allowfullscreen></iframe>`,
-// 		provider_name: 'Streamable',
-// 		provider_url: 'https://streamable.com',
-// 		thumbnail_url:
-// 			'//cdn-cf-east.streamable.com/image/uc9t1z.jpg?Expires=1717837680&Signature=MV4l2ZMgmc-p3YPRFvNI-~a-x0NdL98wWWlUPEW6Tu4TdzsRSw05nEGOVUlvHQplgu30sc0tdSoshiV0Bl3CLWpjANDczjwu6QCdbhBLvUc9F6TP5WWo1j6qxhR8iLnC-cEJP7qAllEOC8XOZNuMtSsO7UpJBlyDB-kBjlaWuhGjk6VRn09V1njWyP5DRuFNsCiqMvUJtjLuAvb7SrUK63HiVV01fA0PyPyV-ATxU5PvePUy7NjLxOaU~HnW9r5byFKJFtAvtdblwCv-tQREgmCRk~c3rqAYz8RBFVDw1l9QrCyCv8xGaZQSQkYevAyBkeNaU5fTMP~QIaIYdzzOuQ__&Key-Pair-Id=APKAIEYUVEN4EVB2OKEQ',
-// 		thumbnail_width: 720,
-// 		thumbnail_height: 1280,
-// 		title: 'test-video',
-// 		type: 'video',
-// 		version: '1.0',
-// 		width: 720
-// 	})
-// 	// res.render('player', { url })
-// })
+app.get('/player', (req: express.Request, res: express.Response): void => {
+	res.json({
+		height: 1280,
+		html: `<iframe class="streamable-embed" src="https://storage.googleapis.com/knock-extension/test-video.mp4" frameborder="0" scrolling="no" width="720" height="1280" allowfullscreen></iframe>`,
+		provider_name: 'Streamable',
+		provider_url: 'https://streamable.com',
+		thumbnail_url:
+			'//cdn-cf-east.streamable.com/image/uc9t1z.jpg?Expires=1717837680&Signature=MV4l2ZMgmc-p3YPRFvNI-~a-x0NdL98wWWlUPEW6Tu4TdzsRSw05nEGOVUlvHQplgu30sc0tdSoshiV0Bl3CLWpjANDczjwu6QCdbhBLvUc9F6TP5WWo1j6qxhR8iLnC-cEJP7qAllEOC8XOZNuMtSsO7UpJBlyDB-kBjlaWuhGjk6VRn09V1njWyP5DRuFNsCiqMvUJtjLuAvb7SrUK63HiVV01fA0PyPyV-ATxU5PvePUy7NjLxOaU~HnW9r5byFKJFtAvtdblwCv-tQREgmCRk~c3rqAYz8RBFVDw1l9QrCyCv8xGaZQSQkYevAyBkeNaU5fTMP~QIaIYdzzOuQ__&Key-Pair-Id=APKAIEYUVEN4EVB2OKEQ',
+		thumbnail_width: 720,
+		thumbnail_height: 1280,
+		title: 'test-video',
+		type: 'video',
+		version: '1.0',
+		width: 720
+	})
+})
 
 app.listen(process.env.PORT || PORT, () => {
 	return console.log(`Express is listening at http://localhost:${PORT}`)
