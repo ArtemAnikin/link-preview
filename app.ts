0fb3c37261d32ac94db72d0cbe25bbd8439c0de6
anik_ad78 <Anikinworkplace@gmail.com>
2024-06-05 14:10:31 +0200

    video

--- app.ts
+++ app.ts
@@ -17,7 +17,7 @@ app.get('/', (req: express.Request, res: express.Response): void => {
 app.get('/oembed', (req, res) => {
 	res.json({
 		height: 1280,
-		html: '<iframe class="streamable-embed" src="https://test-video-23-3a148a2fd5d5.herokuapp.com/player-knock" frameborder="0" scrolling="no" width="720" height="1280" allowfullscreen></iframe>',
+		html: '<iframe class="streamable-embed" src="https://test-video-24-ba35171636fd.herokuapp.com/player-knock" frameborder="0" scrolling="no" width="720" height="1280" allowfullscreen></iframe>',
 		provider_name: 'Streamable',
 		provider_url: 'https://streamable.com',
 		thumbnail_url:
